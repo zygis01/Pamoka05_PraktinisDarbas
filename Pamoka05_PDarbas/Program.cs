0e03325eaebc258adea3ac8beb1558b81300daae
Zygis <zygisje@gmail.com>
2022-09-14 10:52:32 +0300

    Pabaigta praktine uzduotis

--- Pamoka05_PDarbas/Program.cs
+++ Pamoka05_PDarbas/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿Console.WriteLine("STATUSIS TRIKAMPIS");
+Console.WriteLine();
+
+// Apsirašau turimus du kintamuosiuos 
+
+var aKrastine = 10;
+var bKrastine = 15;
+
+// Pasirašau pagal kokia formule skaičiuosiu
+
+Console.WriteLine("Pitagoro teorema - c*2 = b*2 + a*2");
+Console.WriteLine();
+
+// Atlieku matematinį veiksmą pagal pitagoro teoremą
+
+double cKrastine = Math.Sqrt((aKrastine * aKrastine + bKrastine * bKrastine));
+
+// Išvedu gautą atsakymą user'iui
+
+Console.WriteLine($"Jūsų a kraštinės ilgis yra : {aKrastine}");
+Console.WriteLine($"Jūsų b kraštinės ilgis yra : {bKrastine}");
+Console.WriteLine();
+Console.WriteLine($"Jūsų įžambinės ilgis yra : {cKrastine}");
+
+var Kintamasis3 = cKrastine;
